74be7ad12305477bdab16d6f647d7018ed7ba491
Vivek <vivekkesarwani2005@gmail.com>
2024-03-17 22:05:23 +0530

    update

--- index.js
+++ index.js
@@ -3,98 +3,110 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
+let data ={};
 const port = 3000;
 const API_URL = "https://kodessphere-api.vercel.app";
 
 const yourBearerToken = "HxMvgPp";
+const datas = {
+      fan: 3,
+      bulb: 1,
+      led: "#ff0000",
+      ac: {
+        temp: 25,
+        state: 0
+      }
+}
+let message="";
 
-const config = {
-  headers: { Authorization: `Bearer ${yourBearerToken}` },
-};
+function time(){
+  const time = new Date();
+  const hour = time.getHours();
+  if(hour>6 && hour<12)
+  {
+    message="Good Morning !";
+  }
+  else if(hour>12 && hour<18)
+  {
+    message="Good Afternoon !";
+  }
+  else
+  {
+    message="Night Time !"
+  }
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/",  (req, res) => {
+  //const input = axios.get(API_URL + "/devices/" + yourBearerToken);
+  //const data = input.data;
+  console.log(datas);
+  res.render("index.ejs", {datas,message});
 });
 
-app.post("/devices", async (req, res) => {
-  const device = req.body.device;
-  const value = req.body.value;
+app.get("/fan", (req,res) => {
+  res.send("FAN")
+})
+
+app.post("/devices",  (req, res) => {
   console.log(req.body);
+  const device = req.body.device;
+  if(device === "fan")
+  {
+    let value= parseInt(req.body.value);
+    data = {
+    teamid: "HxMvgPp",
+    device: device,
+    value: value
+    }
+  }
+  else if(device === "bulb")
+  {
+    let value = parseInt(req.body.value);
+    data = {
+      teamid: "HxMvgPp",
+      device: device,
+      value: value
+      }
+  }
+  else if(device === "ac")
+  {
+    let temp = parseInt(req.body.value);
+    let state = parseInt(req.body.state);
+    data = {
+      teamid: "HxMvgPp",
+      device: device,
+      value: {
+        "temp": temp,
+        "state": 0
+      }
+      }
+  }
+  else
+  {
+    let color = req.body.color;
+    data = {
+      teamid: "HxMvgPp",
+      device: device,
+      value: color
+      }
+  }
 
-  /*try {
-    const result = await axios.get(API_URL + "/devices/" + searchId, config);
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  console.log(data);
+
+  try {
+    const result = axios.post(API_URL + "/devices", data);
+    console.log(result.data);
+    res.redirect("/");
   } catch (error) {
-    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-  }*/
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-app.post("/post-secret", async (req, res) => {
-  // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
-  const update ={
-    secret: req.body.secret,
-    score: req.body.score
-  };
-  try{
-    const result = await axios.post(API_URL+"/secrets", update , config);
-    res.render("index.ejs", {content : JSON.stringify(result.data) });
-
-  }catch(error){
-    res.render("index.ejs", {content: JSON.stringify(error.response.data)});
-  }
-});
-
-app.post("/put-secret", async (req, res) => {
-  const searchId = req.body.id;
-  // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
-  const update ={
-    secret: req.body.secret,
-    score: req.body.score
-  };
-  try{
-    const result = await axios.put(API_URL+"/secrets/"+searchId , update , config);
-    res.render("index.ejs", {content : JSON.stringify(result.data)});
-  }catch(error){
-    res.render("index.ejs",{content : JSON.stringify(error.response.data)});
+    res.send("Error");
   }
 
-  
 });
-
-app.post("/patch-secret", async (req, res) => {
-  const searchId = req.body.id;
-  // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
-});
-
-app.post("/delete-secret", async (req, res) => {
-  const searchId = req.body.id;
-  // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
-});*/
-
